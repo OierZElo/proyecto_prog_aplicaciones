a913ffa9c4c9ceb8122b24f5ae3d8844818cfa62
Oier Zugazaga <oier.z@opendeusto.es>
2025-10-22 16:19:29 +0200

    test

---
+++ proyecto java test/src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+import javax.swing.SwingUtilities;
+import view.MainFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			MainFrame mainFrame = new MainFrame();
+			mainFrame.setVisible(true);
+			
+		});
+		
+
+
+	}
+
+}
