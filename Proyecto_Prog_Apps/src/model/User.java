7614f0bf843b63573a6d53803246ececcae77286
ekaitzlazkano-ui <ekaitz.lazkano@opendeusto.es>
2025-10-24 11:28:27 +0200

    generateUsers() and getUserFromUsername() in class utils

--- Proyecto_Prog_Apps/src/model/User.java
+++ Proyecto_Prog_Apps/src/model/User.java
@@ -41,7 +41,7 @@ public class User {
 		return id;
 	}
 	
-
+	
 
 
 	
--- Proyecto_Prog_Apps/src/utils/Utils.java
+++ Proyecto_Prog_Apps/src/utils/Utils.java
@@ -1,5 +1,26 @@
 package utils;
 
-public class Utils {
+import java.util.ArrayList;
 
+import model.User;
+
+public class Utils {
+	private ArrayList<User> users = new ArrayList<User>();
+	
+	private void generateUsers() {
+		users.add(new User("user1", "password1"));
+		users.add(new User("user2", "password2"));
+		users.add(new User("user3", "password3"));
+		users.add(new User("user4", "password4"));
+		users.add(new User("user5", "password5"));
+	}
+	
+	private User getUserFromUsername(String username) {
+		for(User user: users) {
+			if (user.getUsername().equals(username)) {
+				return user;
+			}
+		}
+		return null;
+	}
 }
