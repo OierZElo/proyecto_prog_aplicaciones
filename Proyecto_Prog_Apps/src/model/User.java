87e3f616ca4c05594c8fb037235fcc4527694f9c
ekaitzlazkano-ui <ekaitz.lazkano@opendeusto.es>
2025-10-24 10:47:49 +0200

    User class constructor and getters/setters

--- Proyecto_Prog_Apps/src/model/User.java
+++ Proyecto_Prog_Apps/src/model/User.java
@@ -1,5 +1,44 @@
 package model;
 
 public class User {
+	private static int counter = 0;
+	private int cod;
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		this.cod = counter;
+		counter += 1;
+		this.username = username;
+		this.password = password;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+	
+	
+
+
+	
+	
+	
+	
+	
 }
