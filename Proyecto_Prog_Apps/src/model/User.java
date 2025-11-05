779886d6a74c5ce25aa804473d7dc90c496f5525
Daniela <danielavalentina.s@opendeusto.es>
2025-11-05 09:32:17 +0100

    he vuelto a modificar el user para añadir foto de perfil.

--- Proyecto_Prog_Apps/src/model/User.java
+++ Proyecto_Prog_Apps/src/model/User.java
@@ -1,15 +1,19 @@
 package model;
 
+import javax.swing.ImageIcon;
+
 public class User {
 	private String name;
 	private String password;
 	private String mail; 
+	private ImageIcon photo; 
 
 	public User(String mail, String password) {
 		super();
 		this.mail = mail;
 		this.password = password;
 		this.name = null;
+		this.photo = new ImageIcon("src\\resources\\icons\\SongIcon.png"); 
 	}
 
 	public String getName() {
