8b52d02d505bc2ae03371d0ac6ba152666860b96
Daniela <danielavalentina.s@opendeusto.es>
2025-11-06 11:25:04 +0100

    Merge branch 'main' of git@github.com:OierZElo/proyecto_prog_aplicaciones.git into main
    correccion log Out

--- Proyecto_Prog_Apps/src/view/UserPanel.java
+++ Proyecto_Prog_Apps/src/view/UserPanel.java
@@ -128,16 +128,6 @@ public class UserPanel {
 		lg.setBackground(MainFrame.BorderColor);
 		JButton chp = new JButton("Change password "); 
 		chp.setBackground(MainFrame.BorderColor);
-
-		lg.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				LoginRegisterDialog loginFrame = new LoginRegisterDialog();
-				loginFrame.setVisible(true);
-				MainFrame.getInstance().dispose();
-			}
-		});
-		
 		r.add(cp); 
 		r.add(lg);
 		r.add(chp);
@@ -279,6 +269,7 @@ public class UserPanel {
 			public void actionPerformed(ActionEvent e) {
 				LoginRegisterDialog login = new LoginRegisterDialog(); 
 				result.dispose();
+				MainFrame.getInstance().dispose();
 				login.setVisible(true);
 				
 			}
