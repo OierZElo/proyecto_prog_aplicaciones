8b8c70f18f7842a347e4b11fb1f1c1656493e435
ekaitzlazkano-ui <ekaitz.lazkano@opendeusto.es>
2025-12-08 11:46:24 +0100

    fixing Usuario push

--- Proyecto_Prog_Apps/src/model/Genre.java
+++ Proyecto_Prog_Apps/src/model/Genre.java
@@ -1,25 +1,14 @@
 package model;
 
 public enum Genre {
-    ROCK("Rock"),
-    POP("Pop"),
-    JAZZ("Jazz"),
-    CLASICA("Clásica"),
-    HIPHOP("HipHop"),
-    REGGAE("Reggae"),
-    ELECTRONICA("Electrónica"),
-    BLUES("Blues"),
-    COUNTRY("Country"),
-    METAL("Metal");
-
-    private final String displayName;
-
-    Genre(String displayName){
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+    ROCK,
+    POP,
+    JAZZ,
+    CLASICA,
+    HIPHOP,
+    REGGAE,
+    ELECTRONICA,
+    BLUES,
+    COUNTRY,
+    METAL
 }
-
